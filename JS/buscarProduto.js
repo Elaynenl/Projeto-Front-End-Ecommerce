0348671f0c6af5d1b2d5adc78524e18a6635e738
Elaynenl <layninha.nascimento@gmail.com>
2024-05-28 21:27:16 -0300

    Criando mensagem de erro (com javascript) para ser exibida quando a pesquisa realizada não retornar nenhum ítem. Criando botão para retornar para a página com todos os produtos após realizar a pesquisa por palavra-chave. Adicionando  Adicionando o menu Promoções para resoluções a partir de 1728px. Ajustando tamanho do título de produtos em promoção.

--- JS/buscarProduto.js
+++ JS/buscarProduto.js
@@ -1,6 +1,7 @@
 import { conectaApi } from "./conectaApi.js";
 import constroiCard from "./mostrarProdPromo.js";
 
+
 async function buscarProduto(evento) {
     evento.preventDefault();
     
@@ -8,14 +9,26 @@ async function buscarProduto(evento) {
     const busca = await conectaApi.buscaProduto(dadosDePesquisa);
 
     const lista = document.querySelector("[data-lista]");
+    
 
     while (lista.firstChild) {
         lista.removeChild(lista.firstChild);
     }
-
+  
     busca.forEach(elemento => lista.appendChild(
-        constroiCard(elemento.titulo__produto, elemento.imagem, elemento.valor_original, elemento.valor__desconto, elemento.valor__atual)))
+        constroiCard(elemento.titulo__produto, elemento.imagem, elemento.valor_original, elemento.valor__desconto, elemento.valor__atual)));
+    
+        if (busca.length === 0) {
+            lista.innerHTML = `<h2 class="mensagem__erro">Produto não encontrado</h2>`
+        } 
+
+        //limpa o input de pesquisa após o retorno da busca.
+        document.querySelector("[data-pesquisa]").value = '';
+
+        document.querySelector("[data-botao-retornar]").style.display = 'inline-block'
+
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
 botaoDePesquisa.addEventListener("click", evento => buscarProduto(evento));
+
--- JS/mostrarProdPromo.js
+++ JS/mostrarProdPromo.js
@@ -8,11 +8,13 @@ export default function constroiCard(titulo__produto, imagem, valor_original, va
     const produtoEmPromo = document.createElement("li");
     produtoEmPromo.className = "container__produto-promo";
     produtoEmPromo.innerHTML = `<a href="" class="container__destaque__link"><img src="${imagem}" alt="imagem de um frasco de perfume masculino" class="container__destaque__imagem"></a>
-    <h3 class="titulo__produto">${titulo__produto}</h3>
+    <h3 class="titulo__produto titulo__produto__promo">${titulo__produto}</h3>
     <p class="valor"> <s>${valor_original}</s></p>
     <p class="valor__desconto">${valor__desconto}</p>
-    <p class="valor__atual">${valor__atual}</p>
+    <p class="valor__atual valor__desconto__aplicado">${valor__atual}</p>
     <a href="" class="botao__comprar-produto">COMPRAR</a>`
+
+    //classes dos itens da lista aproveitada do código da section Destaques, para ter o mesmo estilo
 
     return produtoEmPromo;
 }
@@ -25,7 +27,7 @@ async function listaProdutos() {
         listaApi.forEach(elemento => lista.appendChild(
             constroiCard(elemento.titulo__produto, elemento.imagem, elemento.valor_original, elemento.valor__desconto, elemento.valor__atual)))
     } catch {
-        lista.innerHTML = `<h2 class="mensagem_titulo">Não foi possível carregar a lista de produtos em promoção</h2>`
+        lista.innerHTML = `<h2 class="mensagem_titulo">Não foi possível carregar a lista de produtos em promoção!</h2>`
     }
 }
 
