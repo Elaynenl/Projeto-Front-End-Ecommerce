02be375b948de8bd0c723466bffd718d619315ed
Elaynenl <layninha.nascimento@gmail.com>
2024-07-02 11:35:17 -0300

    Ajustando a rota "/" e "/produtosEmPromocao"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,33 @@
 import express from "express";
+import path from "path";
 import cadastroLogin from "./usuariosRoutes.js";
-import produtosEmPromocao from "./produtosPromoRoutes.js"
+import produtosEmPromocao from "./produtosPromoRoutes.js";
 import categorias from "./categoriasRoutes.js";
-// import { categoria } from "../models/Categoria.js";
+
 
 const routes = (app) => {
-    app.route("/").get((req,res) => res.status(200).send("Projeto Desenvolve"));
+    
+    const __dirname = path.resolve();
 
-    app.use(express.json(), cadastroLogin, produtosEmPromocao, categorias );
-    // app.use(cadastroLogin);
-    // app.use(produtosEmPromocao);
-    // app.use(categoria);
+    
+    app.route("/").get((req, res) => {
+        res.status(200).sendFile(path.join(__dirname, "index.html"));
+    });
 
+    //ao inserir esse código o arquivo promo.html vai pra rota /produtosEmPromocao, mas os produtos não são mais listados.
+
+    // app.route("/produtosEmPromocao").get((req, res) => {
+    //     res.status(200).sendFile(path.join(__dirname, "HTML/promo.html"));
+    // });
+
+    
+    app.use(express.static(__dirname));
+    app.use('/img', express.static(path.join(__dirname, 'img')));
+    app.use('/JS', express.static(path.join(__dirname, 'JS')));
+    app.use('/styles', express.static(path.join(__dirname, 'styles')));
+
+    app.use(express.json(), cadastroLogin, produtosEmPromocao, categorias);
 }
 
-
 export default routes;
+
--- src/routes/produtosPromoRoutes.js
+++ src/routes/produtosPromoRoutes.js
@@ -5,7 +5,7 @@ const routes  =  express.Router();
 
 routes.get("/produtosEmPromocao", ProdutoPromoController.listarProdutosPromo);
 routes.get("/produtosEmPromocao/busca", ProdutoPromoController.listarProdutosPromoPorPalavraChave); // Rota para busca por palavra-chave
-routes.get("/produtosEmPromocao/busca", ProdutoPromoController.listarProdutosPromoPorCategoria);
+routes.get("/produtosEmPromocao/busca/categoria", ProdutoPromoController.listarProdutosPromoPorCategoria);
 routes.get("/produtosEmPromocao/:id", ProdutoPromoController.listarProdutosPromoPorId);
 routes.post("/produtosEmPromocao", ProdutoPromoController.cadastrarProduto);
 routes.put("/produtosEmPromocao/:id", ProdutoPromoController.atualizarProduto);
